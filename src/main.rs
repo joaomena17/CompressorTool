064a88c53d4a12c73458fb8cc34e433b3df7dc77
joaomena <joao.17.2001@gmail.com>
2023-04-10 00:43:33 +0100

    add project source folder

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+// Compression Tool project using Huffman's algorithm
+// Following the guide: https://codingchallenges.substack.com/p/coding-challenge-3
+
+
+fn main() {
+    // accept file as input and return error if it is not valid
+
+    // open the file
+
+    // determine the occurence of each character
+
+    // verify table: There are 333 occurrences of ‘X’ and 223000 of ‘t’
+
+    // Build a binary tree with these frequencies
+    // https://opendsa-server.cs.vt.edu/ODSA/Books/CS3/html/Huffman.html
+
+    // unit test
+
+    // use the tree to generate the prefix-code table
+    // https://opendsa-server.cs.vt.edu/ODSA/Books/CS3/html/Huffman.html
+
+    // write a header section to the output file
+
+    // encode the text using the code table
+    
+    // write the compressed content of the source text to the output file, appending it after the header
+
+    // read in the header of the encoded file and rebuild the prefix table to decode the text
+
+    // read the body and decode it using the prefix table
+    
+    // write the decoded text back to the specified output file
+
+    // test everything
+
+    // look to add new features or refactor
+}
