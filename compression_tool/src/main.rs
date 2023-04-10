cd80395dc580097dba768a34cce25e1363047469
joaomena <joao.17.2001@gmail.com>
2023-04-10 04:05:38 +0100

    first steps

--- compression_tool/src/main.rs
+++ compression_tool/src/main.rs
@@ -2,16 +2,55 @@
 // Following the guide: https://codingchallenges.substack.com/p/coding-challenge-3
 
 use std::env;
+use std::fs::File;
+
+enum FuncMode {
+    Encode,
+    Decode,
+}
+
 
 fn main() {
     // accept file as input and return error if it is not valid
-    let args: Vec<String> = env::args().collect();
-    dbg!(args);
 
-    println!(args[0]);
-    
-    return;
+    // collect CLI arguments
+    let args: Vec<String> = env::args().collect();
+
+    // validate arguments
+    match args.len(){
+        1 => {
+            panic!("No arguments inserted");
+        }
+
+        2 => {
+            panic!("Insufficent arguments");
+        }
+
+        3 => {
+            match &args[1][..] {
+                "encode" => {
+                    let mode: FuncMode = FuncMode::Encode;
+                }
+                "decode" => {
+                    let mode: FuncMode = FuncMode::Decode;
+                }
+                _ => {
+                    panic!("Invalid mode argument");
+                },
+            }
+        }
+
+        _ => { panic!("Too many arguments"); }
+    }
+
+    // get source file name from arguments
+    let source_name: String = args[2].clone();
+
     // open the file
+    let source_file = match File::open(source_name){
+        Ok(file) => file,
+        Err(err) => panic!("Unable to open the file: {:?}", err),
+    };
 
     // determine the occurence of each character
 
@@ -40,5 +79,7 @@ fn main() {
     // test everything
 
     // look to add new features or refactor
+
+    return;
 }
 
